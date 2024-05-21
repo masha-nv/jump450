55b39be05cdfb40041ae23e3aafa95e3a6dc185d
masha dev <mdevyatlovskaya@oasysic.com>
2024-05-21 13:54:17 -0400

    add ga event on 50+ selection

--- src/components/home/Form/Form.tsx
+++ src/components/home/Form/Form.tsx
@@ -1,10 +1,24 @@
+import { SyntheticEvent, useState } from "react";
 import GradientButton from "../../../shared/components/Buttons/GradientButton/GradientButton";
 import BrandsWeScaled from "../../BrandsWeScaled/BrandsWeScaled";
 import { adTypeMap } from "../Ad/adTypeMap";
 import { AdType } from "../Ad/types";
 import styles from "./Form.module.scss";
+import ReactGA from "react-ga4";
 
 const Form = () => {
+  const [totalSize, setTotalSize] = useState<string>("");
+
+  function handleSetTotalSize(e: SyntheticEvent<HTMLSelectElement>) {
+    setTotalSize(e.currentTarget.value);
+    if (e.currentTarget.value === "50+") {
+      ReactGA.event({
+        category: e.currentTarget.value,
+        action: "50+ selected",
+        label: "50+ selected",
+      });
+    }
+  }
   return (
     <div className={styles.container}>
       <form className={styles.form}>
@@ -21,7 +35,15 @@ const Form = () => {
         <div className={styles.flex}>
           <div className={styles.inputField}>
             <label htmlFor='name'>How large is your company?</label>
-            <input type='text' id='name' placeholder='Total Employee Size' />
+            <select id='name' value={totalSize} onChange={handleSetTotalSize}>
+              <option defaultChecked value=''>
+                Total Employee Size
+              </option>
+              <option value='0-10'>0-10</option>
+              <option value='10-25'>10-25</option>
+              <option value='25-49'>25-49</option>
+              <option value='50+'>50+</option>
+            </select>
           </div>
           <div className={styles.inputField}>
             <label htmlFor='name'>Current Marketing Challenges</label>
