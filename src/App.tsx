6b545bcdbda6545efa10f798ab529211278c77d3
masha dev <mdevyatlovskaya@oasysic.com>
2024-05-22 07:32:15 -0400

    remove layout from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Ad from "./components/home/Ad/Ad";
 import Form from "./components/home/Form/Form";
-import Layout from "./shared/components/Layout/Layout";
 import SplitScreen from "./shared/components/SplitScreen/SplitScreen";
 import "./App.css";
 import ReactGA from "react-ga4";
+import { Route, Routes } from "react-router-dom";
+import Confirmation from "./components/confirmation/Confirmation";
 
 ReactGA.initialize("G-PBCTSGHGNQ");
 ReactGA.send({
@@ -13,9 +14,12 @@ ReactGA.send({
 
 function App() {
   return (
-    <Layout>
-      <SplitScreen left={<Ad />} right={<Form />} />
-    </Layout>
+    <Routes>
+      <Route
+        path='/'
+        element={<SplitScreen left={<Ad />} right={<Form />} />}></Route>
+      <Route path='/confirmation' element={<Confirmation />}></Route>
+    </Routes>
   );
 }
 
